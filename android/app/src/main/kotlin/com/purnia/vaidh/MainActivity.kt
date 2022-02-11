035a6a52d5c7ee68b29fdaf2524590e69d9db2f1
vikashj1 <vikashjha953@gmail.com>
2022-02-11 20:16:15 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/purnia/vaidh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.purnia.vaidh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff053F5E),
+      ),
+      home: HomePage(),
+    );
+  }
+}
